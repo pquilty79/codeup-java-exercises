d28a7484bf5a8f0b67abbc9d3e79454d9e1a13ee
Patrick Quilty <pquilty79@gmail.com>
2022-02-28 12:21:16 -0600

    hello, world

---
+++ src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 42;
+        String myString = "What is the answer to life, the universe, and everything?";
+        System.out.println(myString);
+        System.out.println(myFavoriteNumber);
+    }
+}
+
