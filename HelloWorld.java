152e94a7dbc12876135e11dfe1ec3f8ccb32700d
Patrick Quilty <pquilty79@gmail.com>
2022-02-28 12:29:59 -0600

    new file

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package main;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, World!");
+        System.out.print("Hello, ");
+        System.out.print("World!");
+        System.out.print("\n");
+
+    }
+}
