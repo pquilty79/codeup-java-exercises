b2007de4c9acfae490e6483af821d17c7a716ffb
Patrick Quilty <pquilty79@gmail.com>
2022-03-04 10:15:49 -0600

    finished highlow

--- src/main/HighLow.java
+++ src/main/HighLow.java
@@ -3,38 +3,37 @@ import java.util.Scanner;
 
 public class HighLow {
     public static void main(String[] args) {
-        int guesses = 1;
-        counter(guesses);
+        if(counter(1)) {
+            System.out.println("Game Over");
+        }
     }
-    public static void evaluate(int random) {
+    public static boolean counter(int guesses) {
+        int random = (int) Math.ceil(Math.random() * 100);
         Scanner scanner = new Scanner(System.in);
         System.out.print("Guess a number between 1 and 100:");
+        while (guesses < 10) {
         String guess = scanner.nextLine().trim();
         try {
             if (Integer.parseInt(guess) == random) {
                 System.out.println("GOOD GUESS!");
-                System.out.println("Would you like to play again?");
-                String response = scanner.nextLine().toLowerCase().trim();
-                if (response.equals("y")) {
-                    counter(1);
-                }
-            } else if (Integer.parseInt(guess)  > random) {
+                guesses = 10;
+            } else if (Integer.parseInt(guess) > random) {
                 System.out.println("LOWER");
-            } else if (Integer.parseInt(guess) < random){
+                guesses++;
+                System.out.printf("Guess %d of 10%n", guesses);
+
+            } else if (Integer.parseInt(guess) < random) {
                 System.out.println("HIGHER");
+                guesses++;
+                System.out.printf("Guess %d of 10%n", guesses);
             }
         } catch (NumberFormatException e) {
             System.out.println(guess + " is not a valid input");
-//            evaluate(random);
+            }
         }
+        return true;
     }
-    public static void counter(int guesses) {
-        int random = (int) Math.ceil(Math.random() * 100);
-        do {
-            System.out.printf("Attempt #%d%n", guesses);
-            evaluate(random);
-            guesses++;
-        } while (guesses < 10);
-        System.out.println("Too many attempts. Game Over");
-        }
+
+
 }
+
